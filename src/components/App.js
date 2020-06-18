439f2dd45632b65f1e387c97a4b5110e2836d1f5
Eng Sung <engx03@gmail.com>
2020-06-18 18:18:22 -0400

    added search bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import coronavirus from '../api/coronavirus';
+import SearchBar from './SearchBar';
 
 class App extends Component{
+    state = { data: []};
+
+    async componentDidMount() {
+        const response = await coronavirus.get('/summary');
+        this.setState({ data: response.data });
+    }
+
+    onSearchSubmit = (term) => {
+        console.log(this.state.data);
+    }
 
     render(){
         return (
-            <div>App</div>
+            <div className="ui container">
+                <SearchBar onSubmit={this.onSearchSubmit}/>
+            </div>
         );
     }
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import "./SearchBar.css";
+
+class SearchBar extends Component{
+    state = { term: '' }
+
+    onFromSubmit = event => {
+        event.preventDefault();
+        this.props.onSubmit(this.state.term);
+    }
+
+    render() {
+        return (
+            <div className="ui placeholder segment">
+                <div className="ui icon header">
+                    <i className="search icon"></i>
+                    Search Country
+                </div>
+                <div className="inline">
+                    <form onSubmit={this.onFromSubmit} className="ui form">
+                        <input 
+                            type="text"
+                            value={this.state.term}
+                            onChange={e => this.setState({ term: e.target.value })}
+                            >
+                        </input>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
